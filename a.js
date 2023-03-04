9b5807a05cde23bf5882362f1a9a283d5a91013b
dainnin <36818514+dainnin@users.noreply.github.com>
2023-03-04 18:38:40 -0300

    Add files via upload

--- a.js
+++ a.js
@@ -7,8 +7,9 @@ $(document).ready(function () {
       
       function Harry(){
           
-          
-        if ($(window).width() >=900 ) {
+         var screenWidth2 = screen.width/screen.width*100;
+         var pte = window.innerWidth/screen.width*100;
+        if (pte>=screenWidth2) {
           var lupi=document.getElementById(lupi);
           $("#lupi").css({"font-size":"150%"});
           
@@ -19,3 +20,37 @@ $(document).ready(function () {
           }
       }
   }); 
+           
+$(document).ready(function () {
+      $(window).resize(function () {
+           pezon();
+      });
+      
+     pezon();
+      
+      function pezon(){     
+// Get the size of the device screen
+var screenWidth = screen.width/screen.width*100;
+var screenHeight = window.innerWidth/screen.width*100;
+
+// Get the browser window size
+var windowWidth = window.innerWidth;
+var windowHeight = window.innerHeight;
+
+// Get the size of the entire webpage
+const pageWidth  = document.documentElement.scrollWidth;
+const pageHeight = document.documentElement.scrollHeight;
+
+// Showing the sizes on the webpage
+var x = document.getElementById("screen");
+x.innerHTML = "Device Screen: width: " + screenWidth + ", height: " + screenHeight + ".";
+
+var y = document.getElementById("window");
+y.innerHTML = "Browser Window: width: " + windowWidth + ", height: " + windowHeight + ".";
+
+var z = document.getElementById("page");
+z.innerHTML = "Webpage: width:" + pageWidth + ", height: " + pageHeight + ".";
+}
+
+}); 
+      
