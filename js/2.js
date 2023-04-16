094f20fd3f13c45d25c8171d49aaf44f6850f24f
dainnin <36818514+dainnin@users.noreply.github.com>
2023-04-16 15:15:39 -0300

    Update 2.js

--- js/2.js
+++ js/2.js
@@ -1,91 +1,88 @@
 
 
-async function objList() {
+
 
   const requestURL = 'https://raw.githubusercontent.com/dainnin/proyectowebimg/main/inventario.json';
-  const request = new Request(requestURL);
-
-  const response = await fetch(request);
-  const jsonObjListText = await response.text();
-
-  const jsonObjList = JSON.parse(jsonObjListText);
-  objImglistprsh(jsonObjList);
-  objImglistprvh(jsonObjList);
-}
+     const request = new XMLHttpRequest;
+    request.open('GET', requestURL);
+    request.responseType='json';
+    request.send();
+    request.onload = function () {
+    var obj = request.response;
+    objImglistprvh(obj);
+};
 
 function objImglistprvh(obj) {
-  let i=1;
-  let b=2;
-  let za1=[ "starwars", "pokemon","harryp"];
-  let za = za1[Math.floor(Math.random()*3)] ;
-  console.log(prsho[za][i]["imgz"],za);
-  for (i; i<b , 0!=b%6; ++i,++b) {
+   
+  var i=1;
+  var b=2;
+  var za1=[ "starwars", "pokemon","harryp"];
+  var za = za1[Math.floor(Math.random()*3)] ;
 
-    const contx1 = document.createElement('div');
-    const contx2 = document.createElement('div');
-    const contx3 = document.createElement('div');
-    const he2x = document.createElement('h2');
-    const px = document.createElement('p');
-    const hrx = document.createElement('hr');
-    const ax = document.createElement('a');
-    const imgx1 = document.createElement('img');
-    
-   
-   
-    imgx1.setAttribute("src", obj[za][i]["imgz"]);
-    he2x.textContent = obj[za][0];
-    px.textContent = obj[za][i]["descrip"];
-    ax.setAttribute("href", obj[za][i]["linka"][1]);
-    ax.textContent = obj[za][i]["linka"][0];
-    
 
-   
-    contx1.appendChild(contx2);
-    contx2.appendChild(contx3);
-    contx3.appendChild(he2x);
-    contx3.appendChild(px);
-    contx2.appendChild(hrx);
-    contx2.appendChild(ax);
-    contx1.appendChild(imgx1);
-    
-    prsv.appendChild(contx1);
-}
-}
-function objImglistprsh(obj) {
-  let i=1;
-  let b=2;
-  let za1=[ "starwars", "pokemon","harryp"];
-  let za = za1[Math.floor(Math.random()*3)] ;
+
+for (i; i<b , 0!=b%7; ++i,++b) {
  
-  const prsho = obj;
+  const cont1z = document.createElement('div');
+  const imgxz = document.createElement('img');
+  const he4z = document.createElement('h4');
+  const he3z = document.createElement('h3');
+  const div1z = document.createElement('div');
+  const div2z = document.createElement('div');
+  
  
- for (i; i<b , 0!=b%7; ++i,++b) {
-  console.log(i,b)
-    const cont1 = document.createElement('div');
-    const imgx = document.createElement('img');
-    const he4 = document.createElement('h4');
-    const he3 = document.createElement('h3');
-    const div1 = document.createElement('div');
-    const div2 = document.createElement('div');
-    
-   
+  
+  imgxz.setAttribute("src", obj[za][i]["imgz"]);
+  he4z.textContent = obj[za][0];
+  he3z.textContent = obj[za][i]["nompro"];
+  div1z.textContent = obj[za][i]["precio"];
+  div2z.textContent = obj[za][i]["cuotas"];
 
-    imgx.setAttribute("src", prsho[za][i]["imgz"]);
-    he4.textContent = prsho[za][0];
-    he3.textContent = prsho[za][i]["nompro"];
-    div1.textContent = prsho[za][i]["precio"];
-    div2.textContent = prsho[za][i]["cuotas"];
+ cont1z.appendChild(imgxz);
+  cont1z.appendChild(he4z);
+  cont1z.appendChild(he3z);
+  cont1z.appendChild(div1z);
+  cont1z.appendChild(div2z);
+  
+ 
+  prsh.appendChild(cont1z);
+};
 
-   cont1.appendChild(imgx);
-    cont1.appendChild(he4);
-    cont1.appendChild(he3);
-    cont1.appendChild(div1);
-    cont1.appendChild(div2);
-    console.log(prsho[za][i]["imgz"]);
-   
-    prsh.appendChild(cont1);
-  }
-}
+var ix=1;
+var bx=2;
+var zax = za1[Math.floor(Math.random()*3)] ;
+for (ix; ix<b , 0!=bx%5; ++ix,++bx) {
 
-objList();
+  const contx1 = document.createElement('div');
+  const contx2 = document.createElement('div');
+  const contx3 = document.createElement('div');
+  const he2x = document.createElement('h2');
+  const px = document.createElement('p');
+  const hrx = document.createElement('hr');
+  const ax = document.createElement('a');
+  const imgx1 = document.createElement('img');
+  
+ 
+  
+  imgx1.setAttribute("src", obj[zax][ix]["imgz"]);
+  he2x.textContent = obj[zax][0];
+  px.textContent = obj[zax][ix]["descrip"];
+  ax.setAttribute("href", obj[zax][ix]["linka"][1]);
+  ax.textContent = obj[zax][ix]["linka"][0];
+  
 
+ 
+  contx1.appendChild(contx2);
+  contx2.appendChild(contx3);
+  contx3.appendChild(he2x);
+  contx3.appendChild(px);
+  contx2.appendChild(hrx);
+  contx2.appendChild(ax);
+  contx1.appendChild(imgx1);
+  
+  prsv.appendChild(contx1);
+
+
+ 
+};
+};
