d85244b0a4c4eb13fdd3e03bad83be83a162d70b
dainnin <36818514+dainnin@users.noreply.github.com>
2023-04-25 21:06:18 -0300

    Update productind.js

--- sectores/js/productind.js
+++ sectores/js/productind.js
@@ -17,21 +17,24 @@
  
 
   })
-  function objImglistshop1(obj) {
-    const objT=Object.keys(obj).length-1;
-    
-    var i=1;
-    var b=2;
-    var frcx=Object.keys(obj);
-    
-    let ci=0;
-    const oKT=frcx.length;
-   
-for(ci;ci<oKT ;++ci){
-      
-      for(cix=0;cix<=oKT;++cix){
-      if(obj[frcx[ci]][cix]["nompro"] == titlex.textContent) {
+ function objImglistshop1(obj) {
+  let ci = 0;
+  var frcx = Object.keys(obj);
+  var  nObj=Object.keys(obj).length; 
+  var  nObjp=Object.keys(obj[frcx[ci]]).length;
 
+  
+  
+  
+  
+  
+  
+ console.log(nObj, nObjp);
+  for (ci; ci <nObj; ++ci) {
+   
+    for (cix = 0; cix <nObjp; ++cix) {
+      if (obj[frcx[ci]][cix]["nompro"] == titlex.textContent) {
+        
         const contx1 = document.createElement('div');
         const contx2 = document.createElement('div');
         const contx3 = document.createElement('div');
@@ -40,21 +43,17 @@ for(ci;ci<oKT ;++ci){
         const hrx = document.createElement('hr');
         const ax = document.createElement('a');
         const imgx1 = document.createElement('img');
-        
        
-       
+
         
-      
-        imgx1.setAttribute("src",obj[frcx[ci]][cix]["imgz"]);
+        imgx1.setAttribute("src", obj[frcx[ci]][cix]["imgz"]);
         he2x.textContent = obj[frcx[ci]][cix]["nompro"];
         px.textContent = obj[frcx[ci]][cix]["descrip"];
         ax.setAttribute("href", obj[frcx[ci]][cix]["linka"][1]);
         ax.textContent = obj[frcx[ci]][cix]["linka"][0];
         
-      
-        
-        
-       
+
+
         contx1.appendChild(contx2);
         contx2.appendChild(contx3);
         contx3.appendChild(he2x);
@@ -62,66 +61,63 @@ for(ci;ci<oKT ;++ci){
         contx2.appendChild(hrx);
         contx2.appendChild(ax);
         contx1.appendChild(imgx1);
-        
+
         mainx.appendChild(contx1);
-        
-        
-
-const contz1 = document.createElement('div');
-contz1.setAttribute("class", "dibx");
-contz1.setAttribute("id","harrypx");
-const hez1 = document.createElement('h1');
-hez1.textContent=obj[frcx[ci]][0]["frc"][1];
-const contz2 = document.createElement('div');
-contz2.setAttribute("class", "prsh");
-contz2.setAttribute("id", obj[frcx[ci]][0]["frc"][3] );
-sectionxs.appendChild(contz1);
-contz1.appendChild(hez1) 
-contz1.appendChild(contz2);
 
 
-console.log("Hola");
-  for (i; i<b , 0!=b%7; ++i,++b) {
-      
-    const cont1az = document.createElement('article');
-    const cont1z = document.createElement('a');
-    const imgxz = document.createElement('img');
-    const he4z = document.createElement('h4');
-    const he3z = document.createElement('h3');
-    const div1z = document.createElement('div');
-    const div2z = document.createElement('div');
-      
-     
-    cont1z.setAttribute("href", obj[frcx[ci]][i]["prolink"]);
-      imgxz.setAttribute("src", obj[frcx[ci]][i]["imgz"]);
-      he4z.textContent = obj[frcx[ci]][0]["frc"][0];
-      he3z.textContent = obj[frcx[ci]][i]["nompro"];
-      div1z.textContent = obj[frcx[ci]][i]["precio"];
-      div2z.textContent = obj[frcx[ci]][i]["cuotas"];
-     
-      cont1az.appendChild(cont1z);
-      cont1z.appendChild(imgxz);
-       cont1z.appendChild(he4z);
-       cont1z.appendChild(he3z);
-       cont1z.appendChild(div1z);
-       cont1z.appendChild(div2z);
+
+        const contz1 = document.createElement('div');
+        contz1.setAttribute("class", "dibx");
+        contz1.setAttribute("id", "harrypx");
+        const hez1 = document.createElement('h1');
+        hez1.textContent = obj[frcx[ci]][0]["frc"][1];
+        const contz2 = document.createElement('div');
+        contz2.setAttribute("class", "prsh");
+        contz2.setAttribute("id", obj[frcx[ci]][0]["frc"][3]);
+        sectionxs.appendChild(contz1);
+        contz1.appendChild(hez1)
+        contz1.appendChild(contz2);
          
-       
-        let a69=[(obj[frcx[ci]][0]["frc"][3])];
-        
- document.getElementById(a69).appendChild(cont1az)
-
-
-    }
-
         console.log("Cumplido");
-      } else { }}
-};
-console.log();
- 
+        prsh=[]
+        for(iz=1;iz<nObjp;++iz){
+          prsh.push(iz)
+           }
+           const cixa=(cix)-1;
+           prsh.splice(cixa,1);
+        console.log(prsh,cixa);
+        for (i=1 ,bx=0; i<=prsh.length; i++, bx++) {
+          const cont1az = document.createElement('article');
+          const cont1z = document.createElement('a');
+          const imgxz = document.createElement('img');
+          const he4z = document.createElement('h4');
+          const he3z = document.createElement('h3');
+          const div1z = document.createElement('div');
+          const div2z = document.createElement('div');
+          var frc=Object.keys(obj);
+          
+          
+        
+          imgxz.setAttribute("src", obj[frcx[ci]][prsh[bx]]["imgz"]);
+          cont1z.setAttribute("href", obj[frcx[ci]][prsh[bx]]["prolink"]);;
+          he4z.textContent = obj[frcx[ci]][0]["frc"][0];
+          he3z.textContent = obj[frcx[ci]][prsh[bx]]["nompro"];
+          div1z.textContent = obj[frcx[ci]][prsh[bx]]["precio"];
+          div2z.textContent = obj[frcx[ci]][prsh[bx]]["cuotas"];
+          console.log(prsh[bx]);
+          cont1az.appendChild(cont1z);
+         cont1z.appendChild(imgxz);
+          cont1z.appendChild(he4z);
+          cont1z.appendChild(he3z);
+          cont1z.appendChild(div1z);
+          cont1z.appendChild(div2z);
+           
+          harrypx2.appendChild(cont1az);
+        
+        };
 
-
-
-  }
+      } else { }
+    }
+  };
 
  
