b8cf9a2edd4c5258be8d740c659c2a0aad45aeee
dainnin <36818514+dainnin@users.noreply.github.com>
2023-05-20 00:07:43 -0300

    Update and rename 2.js to principal.js

---
+++ js/principal.js
@@ -0,0 +1,42 @@
+const csl=console.log;
+const linkLocal=location.href;
+
+
+const lectJson2 = async () => {
+  try {
+
+    const resp = await fetch('../proyectowebimg-main/vinculos.json');
+    const jsonObjList = await resp.json();
+    return jsonObjList;
+  } catch (pif) {
+    csl(pif);
+
+  };
+
+};
+ 
+lectJson2().then((obj) => {
+  
+  for(const link of obj){
+  if( link == linkLocal){
+
+const css1=document.createElement('script');
+css1.setAttribute("src","./libreria/indexHTML.js");
+
+document.head.appendChild(css1);   
+    csl("OK y porque ahora si");
+  
+  }else{csl("que paso")};
+}
+})
+
+const css1=document.createElement('link');
+css1.setAttribute("rel","stylesheet");
+css1.setAttribute("href","./css/footer.css");
+
+
+document.head.appendChild(css1);
+
+
+ 
+
