ee277a8bd081e7e1f5f1825821c2eb32c4bf325c
PabloTec <36818514+dainnin@users.noreply.github.com>
2025-04-17 17:26:40 -0300

    Update front.js

--- modulos/front.js
+++ modulos/front.js
@@ -1,16 +1,16 @@
 alert()
 
-import { createUpdate, HashEnabled } from '/dainnin/modulos/funciones/creacion.js'
-import { rutas } from '/dainnin/modulos/enrutador/rutas.js'
-import { footer } from '/dainnin/modulos/staticDOM/footer.js';
-import { header } from '/dainnin/modulos/staticDOM/header.js';
+//import { createUpdate, HashEnabled } from '/dainnin/modulos/funciones/creacion.js'
+//import { rutas } from '/dainnin/modulos/enrutador/rutas.js'
+//import { footer } from '/dainnin/modulos/staticDOM/footer.js';
+//import { header } from '/dainnin/modulos/staticDOM/header.js';
 
 
-HashEnabled();
+//HashEnabled();
 
- await createUpdate(
-    rutas,
-    {
-        header,
-        footer
-    })
+ //await createUpdate(
+    //rutas,
+  //  {
+   //     header,
+ //       footer
+ //   })
