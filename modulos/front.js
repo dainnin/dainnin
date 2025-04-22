e4111d1d4ae4f0d8197bf0f66562b4d1442ec269
PabloTec <36818514+dainnin@users.noreply.github.com>
2025-04-22 12:59:35 -0300

    Update front.js

--- modulos/front.js
+++ modulos/front.js
@@ -1,9 +1,9 @@
-const { createUpdate, HashEnabled } =await import('/modulos/funciones/creacion.js')
+const { createUpdate, HashEnabled } =await import('/dainnin/modulos/funciones/creacion.js')
 HashEnabled();
-const { ab } = await import('/modulos/req/checkToken.js')
-const { rutas } = await import('/modulos/enrutador/rutas.js')
-import { footer } from '/modulos/staticDOM/footer.js';
-const { header } = await import( '/modulos/staticDOM/header.js');
+const { ab } = await import('/dainnin/modulos/req/checkToken.js')
+const { rutas } = await import('/dainnin/modulos/enrutador/rutas.js')
+const { footer } =  await import ('/dainnin/modulos/staticDOM/footer.js');
+const { header } = await import( '/dainnin/modulos/staticDOM/header.js');
 
 
 
