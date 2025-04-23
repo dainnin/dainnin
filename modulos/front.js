c2caf0451c9880a961528ed7254b1db9a40b62bc
PabloTec <36818514+dainnin@users.noreply.github.com>
2025-04-23 12:45:21 -0300

    Update front.js

--- modulos/front.js
+++ modulos/front.js
@@ -1,9 +1,9 @@
-const { createUpdate, HashEnabled } =await import('/modulos/funciones/creacion.js')
+const { createUpdate, HashEnabled } =await import(urls.online.app+'/modulos/funciones/creacion.js')
 HashEnabled();
-const { ab } = await import('/modulos/req/checkToken.js')
-const { rutas } = await import('/modulos/enrutador/rutas.js')
-const { footer } = await import ('/modulos/staticDOM/footer.js');
-const { header } = await import( '/modulos/staticDOM/header.js');
+const { ab } = await import(urls.online.app+'/modulos/req/checkToken.js')
+const { rutas } = await import(urls.online.app+'/modulos/enrutador/rutas.js')
+const { footer } = await import (urls.online.app+'/modulos/staticDOM/footer.js');
+const { header } = await import( urls.online.app+'/modulos/staticDOM/header.js');
 
 
 
