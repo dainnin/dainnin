f5758426890664af4132693be111277c55ced463
PabloTec <36818514+dainnin@users.noreply.github.com>
2025-04-24 12:38:20 -0300

    Update login.js

--- modulos/main/login.js
+++ modulos/main/login.js
@@ -1,7 +1,7 @@
 
-const { HTMLatObj,HTMLatDOM } =await import( `${urls.online.app}/modulos/funciones/creacion.js`)
+const { HTMLatObj } =await import( `${urls.online.app}/modulos/funciones/creacion.js`)
 const { $ } = await import(`${urls.online.app}/modulos/funciones/utilidades.js`);
-const { ab,setStateCheck,setStateArr } =await import (`${urls.online.app}/modulos/req/checkToken.js`)
+const { ab } =await import (`${urls.online.app}/modulos/req/checkToken.js`)
 
 
 
@@ -9,7 +9,7 @@ const { ab,setStateCheck,setStateArr } =await import (`${urls.online.app}/modulo
 async function login(event) {
     event.preventDefault()
     const form = event.target.form
-
+    const name = form.name
     const data = new FormData(form)
     const body = {}
     for (let [k, v] of data.entries()) {
@@ -19,7 +19,7 @@ async function login(event) {
     if (event.target.tagName === "BUTTON" && Object.values(body).indexOf("") === -1) {
         const req = new $.test({});
 
-         req.fetchE([`${urls.online.api}login`, {
+         req.fetchE([`${urls.online.api+name}`, {
             "credentials": "include",
             "headers": {
 
@@ -31,13 +31,14 @@ async function login(event) {
             "body": JSON.stringify(body),
             "method": "POST",
 
-        }]).then(()=>ab.fetchR)
+        }]).then((a)=>{
+a.error===null?alert(name==='login'?a.data.user+" has iniciado con exito":`has ${form.user.value} ${a.data.data} con exito`):alert(a.error)
+a.error===null&&name==='register'?menR.checked=false:''
+ab.fetchR
 
-        
-
+        })
 
     }
-
 
 }
 
@@ -47,22 +48,27 @@ export const LoginForm = () => {
    return HTMLatObj(`
     <link rel="stylesheet" href="${urls.online.app}/modulos/css/login.css"></link>
      <input id="menR" name="menR" type="checkbox"></input><label htmlFor="menR" ></label>
-    <form onclick="registrar||F" className="registrar">
+    <form onclick="login||F" className="registrar" name="register">
             <label>
                 Usuario:
                 <input type="text" name="user"  required ></input>
             </label>
             <label>
+                Nombre:
+                <input type="text" name="nombre"  required></input>
+            </label>
+            <label>
+                Correo:
+                <input type="email" name="email"  required ></input>
+            </label>
+            <label>
                 Password:
                 <input type="password" name="password"  required></input>
             </label>
-             <label>
-                Password:
-                <input type="password" name="password"  required></input>
-            </label>
+             
             <button >Registrar</button>
     </form>
-    <form onclick="login||F">
+    <form onclick="login||F" name="login">
             <label>
                 Usuario:
                 <input type="text" name="user"  required ></input>
