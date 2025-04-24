96d6cd87adaa838f055576d4caf8d365b980cc2b
PabloTec <36818514+dainnin@users.noreply.github.com>
2025-04-24 12:49:42 -0300

    Update login.js

--- modulos/main/login.js
+++ modulos/main/login.js
@@ -32,7 +32,7 @@ async function login(event) {
             "method": "POST",
 
         }]).then((a)=>{
-a.error===null?alert(name==='login'?a.data.user+" has iniciado con exito":`has ${form.user.value} ${a.data.data} con exito`):alert(a.error)
+a.error===null?alert(name==='login'?form.user.value+" has iniciado con exito":`has ${form.user.value} Registrado con exito`):alert('Error al registrar')
 a.error===null&&name==='register'?menR.checked=false:''
 ab.fetchR
 
