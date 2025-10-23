3b90c58ac7571d59d129701a7f62d1a8631c64b7
PabloTec <36818514+dainnin@users.noreply.github.com>
2025-10-23 01:06:24 -0300

    Update map.js

--- config/map.js
+++ config/map.js
@@ -4,8 +4,8 @@ const mapeo = new function () {
   this.obstacles = {};
   const data = {
     tileSize: 48,
-    mapWidth: 1000,
-    mapHeight: 700,
+    mapWidth: 700,
+    mapHeight: 100,
   }
   Object.defineProperties(this, {
     _map: {
@@ -140,4 +140,5 @@ function drawMap(ctx, camera, tileStyles = {}) {
     }
   }
 }
+
 
